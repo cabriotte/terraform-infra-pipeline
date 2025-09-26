392535f270c578060109d28c90af96326ea30239
Rogerio Cabriotte <cabriotte@gmail.com>
2025-09-26 08:22:33 -0300

    criado lambda, s3 notification e glue job

--- infra/lambda_process_files.tf
+++ infra/lambda_process_files.tf
@@ -37,5 +37,5 @@ resource "aws_lambda_function" "challenge2-lambda-s3-process-files" {
   runtime       = "python3.9"
 
   filename         = "lambda_function_payload.zip"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  source_code_hash = filebase64sha256("C:\Users\cabri\Documents\Fiap\challenge2\terraform-infra-pipeline\lambda_function_payload.zip")
 }
