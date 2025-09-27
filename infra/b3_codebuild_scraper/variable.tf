2ad5d8f60e6402f4ea8162ec72c792d52db75863
Rogerio Cabriotte <cabriotte@gmail.com>
2025-09-27 20:42:45 -0300

    criado scraper via code build e configurado permissoes

---
+++ infra/b3_codebuild_scraper/variable.tf
@@ -0,0 +1,23 @@
+variable "bucket_name" {
+  type = string
+}
+
+variable "bucket_arn" {
+  type = string
+}
+
+variable "github_repo_url" {
+  type = string
+}
+
+variable "buildspec_path" {
+  type = string
+}
+
+variable "role_name" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
